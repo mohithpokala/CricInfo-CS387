c6653c8e4e421fbc3f801baa05240d75006726f3
mohithpokala <pokalamohith@gmail.com>
2022-01-29 14:45:39 +0530

    added score comparision

--- new_server/Controllers/index.js
+++ new_server/Controllers/index.js
@@ -1,4 +1,4 @@
 const {bat_stats,bowl_stats,misc_info,umpire_info,player_info,summary} = require('./scoreboard.controller');
 const {matches_info,match_info}=require('./matches.contoller');
-
-module.exports= {bat_stats,bowl_stats,misc_info,umpire_info,player_info,summary,matches_info,match_info}
+const {comp} = require('./scorecomparison.controller');
+module.exports= {bat_stats,bowl_stats,misc_info,umpire_info,player_info,summary,matches_info,match_info,comp}
--- new_server/Routes/index.js
+++ new_server/Routes/index.js
@@ -1,5 +1,5 @@
 const express =require('express');
-const {bat_stats,bowl_stats,misc_info,umpire_info,player_info,summary,matches_info,match_info} = require('../Controllers');
+const {bat_stats,bowl_stats,misc_info,umpire_info,player_info,summary,matches_info,match_info,comp} = require('../Controllers');
 const routes = express.Router();
 
 routes.get('/scorecard/bat_stats/:match_id/:innings_number',bat_stats);
@@ -10,5 +10,6 @@ routes.get('/scorecard/player_info/:match_id/',player_info);
 routes.get('/scorecard/scorecard_summary/:match_id/:team_number',summary);
 routes.get('/matches',matches_info);
 routes.get('/matches/:match_id',match_info)
+routes.get('/scorecard/bowl_stats/:match_id/:innings_number',comp);
 
 module.exports = {routes};
