358e341caedf5eba4f7b5900961a01411b3b62d2
mohithpokala <pokalamohith@gmail.com>
2022-01-31 19:34:22 +0530

    updated index

--- new_server/Routes/index.js
+++ new_server/Routes/index.js
@@ -6,7 +6,7 @@ routes.get('/scorecard/:arg1/:arg2/:arg3',scorecard_data);
 routes.get('/matches',matches_info);
 routes.get('/matches/:match_id',match_info);
 routes.get('/scorecomparision/:match_id/:innings_number',comp);
-routes.get('/top3batsman/:match_id/:innings_number',top3batsman);
+routes.get     ('/top3batsman/:match_id/:innings_number',top3batsman);
 routes.get('/top3bowlers/:match_id/:innings_number',top3bowlers);
 routes.get('./player_full_info/:player_id/:type',player_full_info);
 module.exports = {routes};
