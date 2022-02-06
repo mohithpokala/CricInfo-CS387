0636c2e25018506e63cd95d5b0e18b0567366cd5
mohithpokala <pokalamohith@gmail.com>
2022-02-06 19:21:16 +0530

    fixed queries

--- new_server/Models/database.js
+++ new_server/Models/database.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "df",  host: "localhost",
+  password: "Mohith@2002",  host: "localhost",
   port: 5432,
   database: "postgres"
 });
